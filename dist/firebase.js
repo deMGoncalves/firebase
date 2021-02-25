baa4e8c5580aa1f41460319672473937c122d29f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-25 09:46:55 -0300

    build(dist): automatic new publish

---
+++ dist/firebase.js
@@ -0,0 +1,8 @@
+import env from 'env';
+import firebase from 'firebase/app';
+import dev from "./dev.json";
+import prod from "./prod.json";
+import 'firebase/analytics';
+import 'firebase/performance';
+firebase.initializeApp(env.isProd ? prod : dev);
+export default firebase;
